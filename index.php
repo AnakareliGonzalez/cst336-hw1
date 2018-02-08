c6eeb138dfa55929ec229db198b4b617735fe585
Anakareli Gonzalez <anakgonzalez@csumb.edu>
2018-02-08 07:49:45 +0000

    h1

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        
+        <meta charset = "utf-8"/>
+        <title>Computer Science: Programming Languages</title>
+        
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">"
+        
+    </head>
+<!-- closing head -->
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1 id="myh1">Programming Languages</h1>
+        </header>
+        <nav>
+            <hr width = "50%" />
+            <a href="index.html" style="color:#2874A6">Home</a>
+            <a href="cpp.html">C++</a>
+            <a href="java.html">Java</a>
+            <a href="ruby.html">Python</a>
+        </nav>
+        
+        <main>
+            
+            <figure id= "languages">
+                <img src="https://coderseye.com/wp-content/uploads/which-coding-language-is-best-to-choose-first.jpg?x59762"></img>
+            </figure>
+            
+            <div id="text">
+               <br> What is a programming language? <br/>
+                <p>A programming language is a distinct language that identifies instructions that can assemble any output a person wants it to.
+                These languages usually contain instructions for a computer. Most times, algorithms are implemented, to properly run the programs.
+                Three programming languages that will be covered here are C++, Java, and Ruby.</p>
+                
+                
+                <br> History <br/>
+                <p>Some early history on computer programming was contributed by Ada Lovelace, Alan Turing, Alonzo Church, John von Neumann
+                and many others. These people were either mathmeticians, physicists, or scientists that helped start one of the first computer
+                programs. It wasn't until the 1950's that the first programming languages were developed to communicate instructions to computers.
+                The language was called Plankalkul (Plan Calculus), designed 1943 - 1945 by German civil engineer, Konrad Zuse. However, the
+                language was not implemented until 2000.</p>
+                
+                <p>To learn about more programming languages</p>
+                <input class ="MyButton" type= "button" value= "Click Here" onclick="window.location.href= 'http://regulargeek.com/2012/12/08/the-big-list-of-256-programming-languages/'"/>
+                
+                <br>  </br>
+                Click <a href="https://docs.google.com/document/d/1kP9X5Ew8iky92MhF9JIZWJsq3yJFfam3Fdcz0Hix6j0/edit">here</a> to see where information
+                was obtained.
+                <br /><br />
+                
+                <em>"Any fool can write code that a computer can understand.<strong> Good programmers</strong> write code that humans can 
+                understand"</em><br />-Martin Fowler
+                
+            </div>
+            
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+             <hr>
+            Internet Programming CST-336. 2018&copy; Gonzalez <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
